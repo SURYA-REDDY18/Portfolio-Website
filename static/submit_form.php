9cfaff28e209ab9fd54a9bb67a06a4ae8350f270
Surya Reddy <snalla20@uic.edu>
2024-09-24 21:07:40 -0500

    Added Experience section

---
+++ static/submit_form.php
@@ -0,0 +1,18 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
+
+    // Example email functionality (use your server's configuration)
+    $to = "snalla20@uic.edu";
+    $subject = "New Contact Message from $name";
+    $body = "Name: $name\nEmail: $email\n\nMessage:\n$message";
+
+    if (mail($to, $subject, $body)) {
+        echo "Message sent successfully!";
+    } else {
+        echo "There was a problem sending your message.";
+    }
+}
+?>
